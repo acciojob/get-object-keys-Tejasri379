096a757374424b486e4312e317fa8d1f86d02316
Tejasri379 <tejasri0315@gmail.com>
2026-08-25 17:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const user = {
+    name: "Tejasri",
+    role: "student",
+    phoneNumber: 9550577277,
+    summary : function(){
+        return "Tejasri is a student at AccioJob"
+    }
+}
+function getKeys(obj){
+    let array = []
+for(let key in obj){
+    console.log(key)
+    array.push(key)
+}return array
+}
+console.log(getKeys(user))
 //your JS code here. If required.
